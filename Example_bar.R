1450084ad25e55f3320961dc312ed58084f5462a
Jaishree M <jaishreemali.pharma@gmail.com>
2024-02-18 16:25:07 -0700

    Intial commit

---
+++ Example_bar.R
@@ -0,0 +1,20 @@
+
+
+# Capstone
+# 2024-02-18
+library(ggplot2)
+#Create data
+little_women <-data.frame(
+  Name = c("Meg", "Jo", "Beth", "Amy"),
+  Times.Mentioned=c(683,1355,459,645)
+)
+# Create data
+little_women <-data.frame(
+  Name = c("Meg", "Jo", "Beth", "Amy"),
+  Times.Mentioned=c(683,1355,459,645)
+)
+
+# Barplot
+print(little_women)
+    
+           
